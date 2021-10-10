c5dc5b66e3c734c31dc3d638081e13ac294f4087
CherkashinD <cherkashin.du@gmail.com>
2021-10-11 00:13:39 +0300

    add project

---
+++ Crawler/Crawler/Links.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Xml;
+using HtmlAgilityPack;
+
+namespace Crawler
+{
+    public class Links
+    {
+        public List<string> GetLinksHtml(HtmlDocument document, string baseUrl)
+        {
+            var links = new List<string>();
+
+            foreach (HtmlNode node in document.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                var link = node.Attributes["href"].Value;
+                if (!link.Contains("mailto") && !link.Contains("skype"))
+                {
+                    var absoluteUrl = GetAbsoluteUrlString(baseUrl, link);
+                    if (absoluteUrl.StartsWith(baseUrl))
+                        links.Add(absoluteUrl);
+                }
+            }
+            return links.Distinct().ToList();
+        }
+
+        public List<string> GetLinksSitemap(string url)
+        {
+            var links = new List<string>();
+            var sitemapUrl = url + "/sitemap.xml";
+            var wc = new WebClient();
+            wc.Encoding = Encoding.UTF8;
+            var sitemapString = wc.DownloadString(sitemapUrl);
+            var document = new XmlDocument();
+            document.LoadXml(sitemapString);
+
+            foreach (XmlNode node in document.GetElementsByTagName("url"))
+            {
+                if (node["loc"] != null)
+                {
+                    links.Add(node["loc"].InnerText);
+                }
+            }
+            return links;
+        }
+
+        public string GetAbsoluteUrlString(string baseUrl, string url)
+        {
+            var uri = new Uri(url, UriKind.RelativeOrAbsolute);
+            if (!uri.IsAbsoluteUri)
+                uri = new Uri(new Uri(baseUrl), uri);
+            return uri.ToString();
+        }
+
+        public void ShowLinks(List<string> links)
+        {
+            foreach (var link in links)
+            {
+                Console.WriteLine(link);
+            }
+            Console.WriteLine(Environment.NewLine);
+        }
+
+        public void ShowLinksWithTime(List<string> links)
+        {
+            foreach (var link in links)
+            {
+                Console.WriteLine(link);
+                try
+                {
+                    Console.WriteLine(GetResponseTime(link).Milliseconds + "ms");
+                }
+                catch
+                {
+                    Console.WriteLine("Response time is no available");
+                }
+            }
+            Console.WriteLine(Environment.NewLine);
+        }
+
+        static TimeSpan GetResponseTime(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            var timer = new Stopwatch();
+            timer.Start();
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            response.Close();
+            timer.Stop();
+
+            return timer.Elapsed;
+        }
+    }
+}
---
+++ Crawler/Crawler/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using HtmlAgilityPack;
+
+namespace Crawler
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            string url;
+            Uri uriResult = null;
+            bool result = false;
+            do
+            {
+                Console.WriteLine("Please enter website:");
+                url = Console.ReadLine();
+                if (!url.StartsWith(Uri.UriSchemeHttp) || !url.StartsWith(Uri.UriSchemeHttps))
+                {
+                    Console.WriteLine("Please add scheme: http:// or https://");
+                }
+                else
+                {
+                    result = Uri.TryCreate(url, UriKind.Absolute, out uriResult);
+                }
+
+            }
+            while (!result);
+
+            var link = new Links();
+            var document = new HtmlWeb().Load(uriResult);
+            var linksHtml = link.GetLinksHtml(document, url);
+            var linksSitemap = link.GetLinksSitemap(url);
+
+            Console.WriteLine("Urls FOUNDED IN SITEMAP.XML but not founded after crawling a web site.");
+            link.ShowLinks(linksSitemap.Except(linksHtml).ToList());
+
+            Console.WriteLine("Urls FOUNDED BY CRAWLING THE WEBSITE but not in sitemap.xml");
+            link.ShowLinks(linksHtml.Except(linksSitemap).ToList());
+
+            link.ShowLinksWithTime(linksSitemap.Except(linksHtml).Concat(linksHtml.Except(linksSitemap)).ToList());
+            Console.WriteLine("Urls found after crawling a website: " + linksHtml.Count);
+            Console.WriteLine("Urls found in sitemap: " + linksSitemap.Count);
+        }
+    }
+}
+
